aeb89e5b84b2d8009b0206ebcacdc8c69f05e6e2
Anubhav Mohandas <anubhav.manav147@gmail.com>
2024-04-04 08:25:03 +0530

    updating...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
 import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgFor],
+  imports: [RouterOutlet, NgFor, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'grocery_list';
+  tasks: string[] = [];
+  newTask: string = '';
+
+  addTask() {
+    if (this.newTask.trim()) { // Check if task is not empty
+      this.tasks.push(this.newTask);
+      // Clear the input field after adding the task
+      this.newTask = '';
+    }
+  }
 }
